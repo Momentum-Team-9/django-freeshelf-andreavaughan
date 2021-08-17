6ab5c076cd68441b14c595f85eb3481bb96ec450
andreavaughan <amvaughan16@gmail.com>
2021-08-16 20:53:58 -0400

    added book and categories models

--- books/models.py
+++ books/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.fields.related import ManyToManyField
 
 
 class User(AbstractUser):
@@ -8,3 +9,23 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    url = models.URLField()
+    created_at = models.DateField(auto_now_add=True)
+    categories = models.ManyToManyField(Category, related_name='categories')
+
+    def __str__(self):
+        return f'{self.title}, {self.author}'
